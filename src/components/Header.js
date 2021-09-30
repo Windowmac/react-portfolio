289c9d57b00b9597accda7eef3596a2313c027e7
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-30 13:40:04 -0700

    added header component with a headshot component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import Headshot from './headshot/Headshot';
+
+const Header = () => (
+  <section className="section">
+    <div className="box">
+      <Headshot />
+      <div className="columns is-centered">
+        <div className="column box">
+          <h1>Hello Again!</h1>
+        </div>
+        <div className="column box">
+          <h1>Hello Again!</h1>
+        </div>
+        <div className="column box">
+          <h1>Hello Again!</h1>
+        </div>
+        <div className="column box">
+          <h1>Hello Again!</h1>
+        </div>
+        <div className="column box">
+          <h1>Hello Again!</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Header;
---
+++ src/components/headshot/Headshot.js
@@ -0,0 +1,11 @@
+import './Headshot.css';
+
+const Headshot = () => (
+  <figure>
+    <div>
+      <img id="headshot" src="/images/Headshot-cropped.jpg" alt="Headshot"></img>
+    </div>
+  </figure>
+);
+
+export default Headshot;
