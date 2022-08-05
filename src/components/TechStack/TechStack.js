d8b98ca966c253685f2369119df85097fff31b23
McKendree Strommer <mckendree.strommer@gmail.com>
2022-08-05 13:25:01 -0700

    updated Tech Stack with current skills

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -2,18 +2,18 @@ const TechStack = () => (
   <div>
     <div className="block">
       <h4>
-        <strong style={{color: "whitesmoke"}}>Languages:</strong> Javascript, HTML/CSS, JSX, SQL 
+        <strong style={{color: "whitesmoke"}}>Languages:</strong> Javascript/Typescript, some C#, HTML/CSS, JSX, SQL 
       </h4>
     </div>
     <div className="block">
-      <h4><strong style={{color: "whitesmoke"}}>Applications:</strong> MERN Stack, Serverless Architecture, GraphQL, Firebase, Handlebars</h4>
+      <h4><strong style={{color: "whitesmoke"}}>Applications:</strong> MERN Stack (MongoDB, Express, React, NodeJS), Git, Serverless Architecture, GraphQL, Firebase, RP Automations, AWS Architecture, CI/CD Pipelines</h4>
     </div>
     <div className="block">
       <h4><strong style={{color: "whitesmoke"}}>Bio:</strong> I love working with interesting, passionate people. I enjoy really getting into a project with a team and solving the problems to the very best of our abilities. I have a love for being on the cutting edge of technology - including new coding packages, tools, tech, etc.
       </h4>
     </div>
     <div className="block">
-      <h4>I am currently assisting teaching coding students at University of Oregon's Web Development Bootcamp. I am loving the people, the materials, and driving home the fundamentals of great software in the most modern tech stacks.
+      <h4>I am a Software Engineer at Olive AI and an instructor at University of Oregon's Web Development Bootcamp. I am loving the people, designing custom automations, and driving home the fundamentals of great software in the most modern tech stacks.
       </h4>
     </div>
   </div>
